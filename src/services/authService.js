eba369505a53ccba4c92f7b5e86353fb5ccc8716
Connor Southwell <south1cr@gmail.com>
2023-06-12 20:39:55 -0400

    update font awesonme

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,29 +1,18 @@
 import axios from "axios";
 import { baseUrl } from "./baseUrl";
-import { handle401 } from "./handle401";
 
 export const get = (route) => {
   let token = localStorage.getItem("authToken");
 
-  const promise= axios.get(baseUrl + route, {
+  return axios.get(baseUrl + route, {
     headers: { Authorization: `Bearer ${token}` }
   })
-  .catch((err) => {
-    handle401(err)
-    console.log(route, err);
-  })
-  return promise;
 };
 
 export const post = (route, body) => {
   let token = localStorage.getItem("authToken");
 
-  const promise = axios.post(baseUrl + route, body, {
+  return axios.post(baseUrl + route, body, {
     headers: { Authorization: `Bearer ${token}` },
   })
-  /*.catch((err) => {
-    handle401(err)
-    console.log(route, err);
-  })*/
-  return promise;
 };
